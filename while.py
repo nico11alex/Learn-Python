2a88cec149e3ec2e5d8cb9fdb0ce2a1e478af351
nico11alex <rodriguezvillotanicolas@gmail.com>
2026-04-30 23:06:25 -0500

    Bucle While

---
+++ while.py
@@ -0,0 +1,32 @@
+contador = 1
+while contador <= 5:
+    print(contador)
+    contador += 1
+
+print("¡Fin de la ejecución")
+
+clave = input("Ingrese la palabra clave para salir del programa:(salir)")
+while clave != "salir":
+    print("Esa no es la palabra")
+    clave = input("Ingrese la palabra clave para salir del programa:")
+
+print("SALISTE DEL BUCLE")
+
+#ACTIVIDAD
+
+suma = 0
+contador = 0
+while True:
+    num= int(input("Ingrese un número: "))
+    if num == 0:
+        break
+    else:
+        suma += num
+        contador += 1
+
+if contador > 0 :
+    promedio = suma/contador
+
+print(suma)
+print(contador)
+print(promedio)
