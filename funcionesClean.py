ea1658208219b3d9614a8b87c12700b7eff8f65c
nico11alex <rodriguezvillotanicolas@gmail.com>
2026-05-18 22:13:02 -0500

    funciones

---
+++ funcionesClean.py
@@ -0,0 +1,21 @@
+def total_producto(precio,cantidad):
+    return precio * cantidad
+
+def datos_validos(precio,cantidad):
+    return precio > 0 and cantidad > 0
+
+try:
+    producto = input("Producto: ")
+    precio = float(input("Precio: "))
+    cantidad = int(input("Cantidad: "))
+    if datos_validos(precio,cantidad):
+        total = total_producto(precio,cantidad)
+        print(f"Producto: {producto}")
+        print(f"Total: {total}")
+    else:
+        print("Error: valores invalidos")
+except ValueError:
+    print("Error tipo de dato erroneo")
+
+ 
+
