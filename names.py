e91b392d1c34abe76a184ed61baef699142ed913
Nicolas Alexander Rodriguez Villota <rodriguezvillotanicolas@gmail.com>
2026-08-12 10:31:34 -0500

    trabajo del libro python

---
+++ names.py
@@ -0,0 +1,11 @@
+names = ['Cesar', 'Guzman','Camilo', 'Maicol','Daniel','Ederley','Ozuna','Diego','Carlos']
+
+print(f'Mi primer amigo es {names[0].title()}')
+print(f'Mi segundo amigo es {names[1].title()}')
+print(f'Mi tercero amigo es {names[2].title()}')
+print(f'Mi cuarto amigo es {names[3].title()}')
+print(f'Mi quinto amigo es {names[4].title()}')
+print(f'Mi sexto amigo es {names[5].title()}')
+print(f'Mi septimo amigo es {names[6].title()}')
+print(f'Mi Octavo amigo es {names[7].title()}')
+print(f'Mi Noveno amigo es {names[8].title()}')
