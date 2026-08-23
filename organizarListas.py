ec4eb74dce0b303b1bcfef907cebbaad55cca50c
Nicolas Alexander Rodriguez Villota <rodriguezvillotanicolas@gmail.com>
2026-08-22 20:49:48 -0500

    ejercicio de listas

---
+++ organizarListas.py
@@ -0,0 +1,42 @@
+places = ['roma', 'otawa', 'paris', 'san francisco','madrid']
+
+print("\n Lista original")
+print(places)
+
+print("\n Litsa organizada por orden alfabetico")
+print(sorted(places))
+
+print("\n Lista original denuevo")
+print(places)
+
+print("\n Lista organizada por orden alfabetico inverso")
+print(sorted(places,reverse=True))
+
+print("\n Lista original otra vez")
+print(places)
+
+print("\n Lista orden inverso")
+places.reverse()
+print(places)
+
+print("\n Lista original otra vez")
+places.reverse()
+print(places)
+
+print("\n Lista organizada en orden alfabetico")
+places.sort()
+print(places)
+
+print("\n Lista organizada en orden alfabetico inverso")
+places.sort(reverse=True)
+print(places)
+
+print("\nLista de cualquier cosa")
+
+lista = ["basquetball", "futbol", "leer", "habitos atomicos"]
+
+print(lista)
+
+lista.sort()
+print("\nLista organizada en orden alfabetico")
+print(lista)
